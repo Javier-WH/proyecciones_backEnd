3b923195043a99dccbe7382daf616575b1724402
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-01-20 09:14:56 -0400

    cambios en el UPTLL

--- src/backEnd/proyeccion/getPNFPensum.js
+++ src/backEnd/proyeccion/getPNFPensum.js
@@ -1,76 +1,75 @@
-import Pensum from '#models/pensum.js'
-import Pnf from '#models/pnf.js'
-import Trayecto from '#models/trayecto.js'
-import Subject from '#models/subjects.js'
+import Pensum from "#models/pensum.js";
+import Pnf from "#models/pnf.js";
+import Trayecto from "#models/trayecto.js";
+import Subject from "#models/subjects.js";
 
-import { Sequelize } from 'sequelize'
-export default async function getPNFPensum (req, res) {
-  const { pnf: pnfId, trayecto: trayectoId } = req.params
+import { Sequelize } from "sequelize";
+export default async function getPNFPensum(req, res) {
+  const { pnf: pnfId, trayecto: trayectoId } = req.params;
 
   const pnf = await Pnf.findOne({
     where: { id: pnfId },
-    raw: true
-  })
+    raw: true,
+  });
   if (!pnf) {
-    res.json({ error: true, message: 'No se ha encontrado el pnf' })
-    return
+    res.json({ error: true, message: "No se ha encontrado el pnf" });
+    return;
   }
-  const pnfName = pnf.name
+  const pnfName = pnf.name;
 
   const trayectoData = await Trayecto.findOne({
     where: { id: trayectoId },
-    raw: true
-  })
+    raw: true,
+  });
   if (!trayectoData) {
-    res.json({ error: true, message: 'No se ha encontrado el trayecto' })
-    return
+    res.json({ error: true, message: "No se ha encontrado el trayecto" });
+    return;
   }
 
-  const trayectoName = trayectoData.name
+  const trayectoName = trayectoData.name;
 
   const pensums = await Pensum.findAll({
     where: {
       pnf_id: pnfId,
       trayecto_id: trayectoId,
-      active: true
+      active: true,
     },
     include: [
       {
         model: Subject,
-        attributes: []
+        attributes: [],
       },
       {
         model: Trayecto,
         attributes: [],
-        as: 'trayecto'
-      }
+        as: "trayecto",
+      },
     ],
     attributes: [
-      'id',
-      'subject_id',
-      'hours',
-      'quarter',
-      [Sequelize.col('Subject.name'), 'subject'],
-      [Sequelize.col('trayecto.saga_id'), 'trayecto_saga_id']
+      "id",
+      "subject_id",
+      "hours",
+      "quarter",
+      [Sequelize.col("subject.name"), "subject"],
+      [Sequelize.col("trayecto.saga_id"), "trayecto_saga_id"],
     ],
-    raw: true
-  })
+    raw: true,
+  });
   if (pensums.length === 0) {
-    res.json({ error: true, message: 'No se ha encontrado el pensum' })
-    return
+    res.json({ error: true, message: "No se ha encontrado el pensum" });
+    return;
   }
 
-  res.json(
-    {
-      error: false,
-      message: null,
-      data: {
-        pnfName,
-        pnfId,
-        trayectoId,
-        trayectoName,
-        pensums
-      }
-    }
-  )
+  res.json({
+    error: false,
+    message: null,
+    data: {
+      pnfName,
+      pnfId,
+      trayectoId,
+      trayectoName,
+      pensums,
+    },
+  });
 }
+
