57137214671418454653efe80feeb717f5d84d2d
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-01-23 10:21:52 -0400

    endpoints de delete profile agregados

--- src/backEnd/dataBase/querys/profile/deleteProfile.js
+++ src/backEnd/dataBase/querys/profile/deleteProfile.js
@@ -1,14 +1,41 @@
 import Profile from "#models/perfil.js";
+import ProfileNames from "#models/perfilNames.js";
+import connection from "#dataBaseConnection";
 
 async function deleteProfile(req, res) {
-  const { id } = req.params;
-  const result = await Profile.destroy({
-    where: {
-      id,
-    },
-  });
+  const { perfil_name_id } = req.params;
+  if (!perfil_name_id) {
+    return res.status(401).json({ error: "debe suministrar un id de nombre de perfil" });
+  }
 
-  res.json(result);
+  const transaction = await connection.transaction();
+
+  try {
+    await Profile.destroy(
+      {
+        where: {
+          perfil_name_id,
+        },
+      },
+      { transaction }
+    );
+
+    await ProfileNames.destroy(
+      {
+        where: {
+          id: perfil_name_id,
+        },
+      },
+      { transaction }
+    );
+
+    await transaction.commit();
+    res.status(200).json({ message: "perfil eliminado" });
+  } catch (error) {
+    console.log(error);
+    await transaction.rollback();
+    res.status(500).json({ error: "ha ocurrido un error" });
+  }
 }
 export default deleteProfile;
 
--- src/backEnd/routes/routes.js
+++ src/backEnd/routes/routes.js
@@ -1,95 +1,96 @@
-import express from 'express'
-import { fileURLToPath } from 'url'
-import path from 'path'
-import { getPnfs } from '#querys/pnf/getPnf.js'
-import { getSimpleSubjectList } from '#querys/subjects/getSimpleSubjectList.js'
-import { getTrayectos, puTrayecto, postTrayecto, deleteTrayecto } from '#querys/trayecto/getTrayectos.js'
-import getInscriptionData from '#proyeccion/getInscriptionData.js'
-import getPNFPensum from '#proyeccion/getPNFPensum.js'
-import getProfile from '#querys/profile/getProfile.js'
-import getProfileById from '#querys/profile/getProfileById.js'
-import getPerfilNames from '#querys/profile/getProfileNames.js'
-import setProfile from '#querys/profile/postProfile.js'
-import Turnos from '#models/turnos.js'
-import deleteProfile from '#querys/profile/deleteProfile.js'
-import addSubjectToProfile from '#querys/profile/addSubjectToPerfil.js'
-import getSimpleData from '#querys/simpleData/simpleData.js'
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import { getPnfs } from "#querys/pnf/getPnf.js";
+import { getSimpleSubjectList } from "#querys/subjects/getSimpleSubjectList.js";
+import { getTrayectos, puTrayecto, postTrayecto, deleteTrayecto } from "#querys/trayecto/getTrayectos.js";
+import getInscriptionData from "#proyeccion/getInscriptionData.js";
+import getPNFPensum from "#proyeccion/getPNFPensum.js";
+import getProfile from "#querys/profile/getProfile.js";
+import getProfileById from "#querys/profile/getProfileById.js";
+import getPerfilNames from "#querys/profile/getProfileNames.js";
+import setProfile from "#querys/profile/postProfile.js";
+import Turnos from "#models/turnos.js";
+import deleteProfile from "#querys/profile/deleteProfile.js";
+import addSubjectToProfile from "#querys/profile/addSubjectToPerfil.js";
+import getSimpleData from "#querys/simpleData/simpleData.js";
 
-const Router = express.Router()
+const Router = express.Router();
 
 // Obtener la ruta absoluta del directorio actual
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-Router.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'frontEnd', 'index.html'))
-})
+Router.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "frontEnd", "index.html"));
+});
 
-Router.get('/pnfs', async (_, res) => {
-  const pnfs = await getPnfs()
-  res.json(pnfs)
-})
+Router.get("/pnfs", async (_, res) => {
+  const pnfs = await getPnfs();
+  res.json(pnfs);
+});
 
-Router.get('/subjects', async (_, res) => {
-  const subjects = await getSimpleSubjectList()
-  res.json(subjects)
-})
+Router.get("/subjects", async (_, res) => {
+  const subjects = await getSimpleSubjectList();
+  res.json(subjects);
+});
 
-Router.get('/trayectos', async (_, res) => {
-  const trayectos = await getTrayectos()
-  res.json(trayectos)
-})
+Router.get("/trayectos", async (_, res) => {
+  const trayectos = await getTrayectos();
+  res.json(trayectos);
+});
 
-Router.get('/turnos', async (_, res) => {
-  const turnos = await Turnos.findAll({ raw: true })
-  res.json(turnos)
-})
+Router.get("/turnos", async (_, res) => {
+  const turnos = await Turnos.findAll({ raw: true });
+  res.json(turnos);
+});
 
-Router.get('/proyecciones/inscriptionData/:pnf/:trayecto', getInscriptionData)
+Router.get("/proyecciones/inscriptionData/:pnf/:trayecto", getInscriptionData);
 
-Router.get('/proyecciones/pensum/:pnf/:trayecto', getPNFPensum)
+Router.get("/proyecciones/pensum/:pnf/:trayecto", getPNFPensum);
 
-Router.get('/profiles', async (_, res) => {
-  const profiles = await getProfile()
-  res.json(profiles)
-})
+Router.get("/profiles", async (_, res) => {
+  const profiles = await getProfile();
+  res.json(profiles);
+});
 
-Router.get('/profile/:id', getProfileById)
+Router.get("/profile/:id", getProfileById);
 
-Router.get('/profileNames', async (_, res) => {
-  const profileNames = await getPerfilNames()
-  res.json(profileNames)
-})
+Router.get("/profileNames", async (_, res) => {
+  const profileNames = await getPerfilNames();
+  res.json(profileNames);
+});
 
-Router.get('/simpleData', getSimpleData)
+Router.get("/simpleData", getSimpleData);
 
 /// ///////////put
 
-Router.put('/trayectos', express.json(), async (req, res) => {
-  const trayectos = await puTrayecto(req.body)
-  res.json(trayectos)
-})
+Router.put("/trayectos", express.json(), async (req, res) => {
+  const trayectos = await puTrayecto(req.body);
+  res.json(trayectos);
+});
 
 // ///////////post
-Router.post('/trayectos', express.json(), async (req, res) => {
-  const trayectos = await postTrayecto(req.body)
-  res.json(trayectos)
-})
+Router.post("/trayectos", express.json(), async (req, res) => {
+  const trayectos = await postTrayecto(req.body);
+  res.json(trayectos);
+});
 
-Router.post('/profile', express.json(), async (req, res) => {
-  const response = await setProfile(req.body)
-  res.send(response)
-})
+Router.post("/profile", express.json(), async (req, res) => {
+  const response = await setProfile(req.body);
+  res.send(response);
+});
 
-Router.post('/profile/addSubject', express.json(), addSubjectToProfile)
+Router.post("/profile/addSubject", express.json(), addSubjectToProfile);
 
 // //////////delete
 
-Router.delete('/trayectos', express.json(), async (req, res) => {
-  const trayectos = await deleteTrayecto(req.body)
-  res.json(trayectos)
-})
+Router.delete("/trayectos", express.json(), async (req, res) => {
+  const trayectos = await deleteTrayecto(req.body);
+  res.json(trayectos);
+});
 
-Router.delete('/profile/:id', deleteProfile)
+Router.delete("/profile/:perfil_name_id", deleteProfile);
 
-export default Router
+export default Router;
+
