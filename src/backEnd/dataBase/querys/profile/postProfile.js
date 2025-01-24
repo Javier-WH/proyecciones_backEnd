e04d14642559332579e37a3edb05a5c2ff1bd2bd
fj_rh@hotmail.com <javier@andinotechnologies.com>
2025-01-24 10:03:22 -0400

    endpints para agregar/editar el profesor agregados

--- src/backEnd/dataBase/querys/profile/postProfile.js
+++ src/backEnd/dataBase/querys/profile/postProfile.js
@@ -1,17 +1,16 @@
-import PerfilNames from "#models/perfilNames.js";
-import { v4 as uuidv4 } from "uuid";
+import PerfilNames from '#models/perfilNames.js'
+import { v4 as uuidv4 } from 'uuid'
 
-export default async function setProfile({ name, description }) {
-  console.log({ name, description });
-  if (!name || !description) return false;
+export default async function setProfile ({ name, description }) {
+  console.log({ name, description })
+  if (!name || !description) return false
 
-  const id = uuidv4();
+  const id = uuidv4()
   try {
-    await PerfilNames.create({ id, name, description });
-    return true;
+    await PerfilNames.create({ id, name, description })
+    return true
   } catch (error) {
-    console.log(error);
-    return false;
+    console.log(error)
+    return false
   }
 }
-
