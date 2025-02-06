98ee25bd74599078d5c5cc0d17e2ca42ecca2d50
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-02-06 10:20:36 -0400

    cambios al socket para enviar el nombre y el id de la proyeccion

--- src/backEnd/socket/socket.js
+++ src/backEnd/socket/socket.js
@@ -3,7 +3,7 @@ import validateTeacherData from "#utils/validateTeacherData.js";
 import validateSubjectData from "#utils/validateSubject.js";
 import getTeacherList from "#querys/teachers/getTeacherList.js";
 import getSubjectList from "#querys/subjects/getSubjectList.js";
-import { getTeacherData } from "./socketUtils.js";
+import { getTeacherData, checkIfProyectionExists } from "./socketUtils.js";
 import { updateProyection } from "../dataBase/create/updateProyection.js";
 const currentProyectionId = "0b4bb3dc-db19-4e1e-a412-a8fa3f2d8809";
 
@@ -22,6 +22,26 @@ let subjects = [];
 // array de proyecciones realizadas
 let proyectionsDone = [];
 
+// Nombre de la proyección
+let proyectionName = "desconocido";
+let proyectionId = null;
+
+// Verificar si hay una proyeccion activa
+const loadProyection = async () => {
+  const request = await checkIfProyectionExists(currentProyectionId);
+  if (request.error) {
+    console.log(request.message);
+    setTeacherList();
+    return;
+  }
+  proyectionName = request.data.name;
+  proyectionId = request.data.id;
+  const proyeccion = request.data;
+  teachers = await JSON.parse(proyeccion.teachers);
+  subjects = await JSON.parse(proyeccion.subjects);
+  proyectionsDone = await JSON.parse(proyeccion.proyections_done);
+};
+
 export async function setTeacherList() {
   const teacherList = await getTeacherList();
 
@@ -31,14 +51,6 @@ export async function setTeacherList() {
     q3: [...teacherList],
   };
   io?.emit("updateTeachers", teachers);
-}
-
-export function setSubjectList() {
-  getSubjectList().then((data) => {
-    // console.log(data)
-    subjects = [...data];
-    io?.emit("updateSubjects", subjects);
-  });
 }
 
 export default function setupSocket(server) {
@@ -56,6 +68,7 @@ export default function setupSocket(server) {
     socket.emit("updateTeachers", teachers);
     socket.emit("updateSubjects", subjects);
     socket.emit("proyectionsDone", proyectionsDone);
+    socket.emit("proyectionData", { proyectionName, proyectionId });
 
     // Escuchar eventos de actualización de profesores
     socket.on("updateTeachers", (newTeachers) => {
@@ -66,7 +79,6 @@ export default function setupSocket(server) {
       } */
 
       teachers = newTeachers;
-
       updateProyection({
         id: currentProyectionId,
         teachers: JSON.stringify(teachers),
@@ -119,6 +131,5 @@ export default function setupSocket(server) {
   return io;
 }
 
-setTeacherList();
-// setSubjectList()
+loadProyection();
 
