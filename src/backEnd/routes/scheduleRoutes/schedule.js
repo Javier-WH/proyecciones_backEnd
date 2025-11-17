631e1e53c4988a4da3cb71873cc600c904854001
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2025-11-17 11:13:29 -0400

    xx

--- src/backEnd/routes/scheduleRoutes/schedule.js
+++ src/backEnd/routes/scheduleRoutes/schedule.js
@@ -4,24 +4,6 @@ import { Op } from "sequelize";
 
 const Router = express.Router();
 
-Router.get("/schedule", async (_, res) => {
-  const schedule = await Schedule.findAll({ raw: true });
-  if (schedule.length === 0) {
-    return res.status(404).json({ error: true, message: "no se encontraron horarios" });
-  }
-  res.json(schedule);
-});
-
-Router.delete("/schedule", async (req, res) => {
-  try {
-    await Schedule.destroy({ where: { id: { [Op.ne]: null } } });
-    res.status(200).json({ messages: "Horarios eliminados correctamente" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al intentar eliminar los horarios" });
-  }
-});
-
 Router.post("/schedule", express.json(), async (req, res) => {
   try {
     const inserLlist = req.body.schedule.filter((item) => item.id === undefined);
@@ -55,7 +37,16 @@ Router.post("/schedule", express.json(), async (req, res) => {
   }
 });
 
-Router.delete("/", async (req, res) => {
+/*
+Router.get("/schedule", async (_, res) => {
+  const schedule = await Schedule.findAll({ raw: true });
+  if (schedule.length === 0) {
+    return res.status(404).json({ error: true, message: "no se encontraron horarios" });
+  }
+  res.json(schedule);
+});
+
+Router.delete("/schedule", async (req, res) => {
   try {
     await Schedule.destroy({ where: { id: { [Op.ne]: null } } });
     res.status(200).json({ messages: "Horarios eliminados correctamente" });
@@ -64,6 +55,18 @@ Router.delete("/", async (req, res) => {
     res.status(500).json({ error: "Error al intentar eliminar los horarios" });
   }
 });
+
+
+
+Router.delete("/", async (req, res) => {
+  try {
+    await Schedule.destroy({ where: { id: { [Op.ne]: null } } });
+    res.status(200).json({ messages: "Horarios eliminados correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al intentar eliminar los horarios" });
+  }
+});*/
 
 export default Router;
 
